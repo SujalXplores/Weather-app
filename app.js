e6a97c3a6f159a8cb54f8ab228bc341de5a1409b
SujalShah3234 <shahc9437@gmail.com>
2022-02-17 12:19:05 +0530

    🥸 Handlebar integration

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+let template = Handlebars.templates.weather_cards;
 let time_id = 0;
 let form = document.getElementById('get-info-form');
 
@@ -37,37 +38,26 @@ const handleFormSubmit = (event) => {
 };
 
 const updateUI = (current, location) => {
-  let cardTitle = document.getElementsByClassName('card-title');
-  let weather_icon = document.getElementsByClassName('weather-icon');
-  let weather_type = document.getElementsByClassName('weather-type');
-  let temperature = document.getElementsByClassName('temperature');
-
-  let last_updated = document.getElementsByClassName('last-updated');
-  let uv_index = document.getElementsByClassName('uv-index');
-  let pressure = document.getElementsByClassName('pressure');
-  let visibility = document.getElementsByClassName('visibility');
-  let wind_speed = document.getElementsByClassName('wind-speed');
-  let wind_direction = document.getElementsByClassName('wind-direction');
-  let wind_degree = document.getElementsByClassName('wind-degree');
-  let humidity = document.getElementsByClassName('humidity');
-
   const [iconClass, weatherDescription] = weatherDescriptionToClass(
     current.condition.text
   );
 
-  cardTitle[0].innerHTML = `${location.name}, ${location.region}, ${location.country}`;
-  weather_icon[0].src = `https://weatherstack.com/site_images/weather_icon_${iconClass}.svg`;
-  weather_type[0].innerHTML = weatherDescription;
-  temperature[0].innerHTML = `${current.temp_c}&deg;C`;
-
-  last_updated[0].innerHTML = `Last Updated: ${current.last_updated}`;
-  uv_index[0].innerHTML = `UV Index: ${current.uv}`;
-  pressure[0].innerHTML = `Pressure: ${current.pressure_mb}mb`;
-  visibility[0].innerHTML = `Visibility: ${current.vis_km}`;//
-  wind_speed[0].innerHTML = `Wind Speed: ${current.wind_kph} kph`;
-  wind_direction[0].innerHTML = `Wind Direction: ${current.wind_dir}`;
-  wind_degree[0].innerHTML = `Wind Degree: ${current.wind_degree}`;
-  humidity[0].innerHTML = `Humidity: ${current.humidity}`;
+  document.getElementById('weather-cards').innerHTML = template({
+    location: location.name,
+    region: location.region,
+    country: location.country,
+    icon: iconClass,
+    weather: weatherDescription,
+    temperature: current.temp_c,
+    last_updated: current.last_updated,
+    uv_index: current.uv,
+    pressure: current.pressure_mb,
+    visibility: current.vis_km,
+    wind_speed: current.wind_kph,
+    wind_direction: current.wind_dir,
+    wind_degree: current.wind_degree,
+    humidity: current.humidity,
+  });
 
   document.querySelector('.weather-card').classList.remove('d-none');
   form.reset();
